7e3d7048d4489b650a5e7e13e03cb80ad617aa1e
HF XU <hfxu@HFdeMacBook-Pro.local>
2021-01-11 19:43:52 -0700

    las commit

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ get_new_link = requests.get("https://www.google.com")
 print(get_new_link.content)
 print(get_new_link.status_code)
 
-new_repo_url = requests.get('https://raw.githubusercontent.com/xuhf0429/CMPUT404_lab1/main/script.py')
+new_repo_url = requests.get('https://raw.githubusercontent.com/xuhf0429/CMPUT404-lab1/main/script.py')
 print(new_repo_url.status_code)
 print(new_repo_url.text)
 
