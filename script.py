b0987789e5b3514e35478f90ca8d8046aea89b35
HF XU <hfxu@HFdeMacBook-Pro.local>
2021-01-13 19:40:12 -0700

    should be done

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import requests
 print("> requests version: %s" % requests.__version__)
 
 get_new_link = requests.get("https://www.google.com")
-print(get_new_link.content)
+print(get_new_link.text)
 print(get_new_link.status_code)
 
 new_repo_url = requests.get('https://raw.githubusercontent.com/xuhf0429/CMPUT404-lab1/main/script.py')
